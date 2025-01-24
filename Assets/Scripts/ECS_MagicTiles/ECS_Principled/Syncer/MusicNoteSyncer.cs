bbd24b3e0c02a36824890d23d9d057dd5df79940
Duy Vo Tran <votranduy2001@gmail.com>
2025-01-24 20:48:58 +0700

    Complete input mechanism

--- Assets/Scripts/ECS_MagicTiles/ECS_Principled/Syncer/MusicNoteSyncer.cs
+++ Assets/Scripts/ECS_MagicTiles/ECS_Principled/Syncer/MusicNoteSyncer.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework.Constraints;
+using ECS_MagicTile.Components;
 using UnityEngine;
 
 namespace ECS_MagicTile
@@ -32,12 +30,17 @@ namespace ECS_MagicTile
         public override void Update(float deltaTime)
         {
             base.Update(deltaTime);
-            MusicNoteComponent[] notes = DedicatedStorage.GetComponents<MusicNoteComponent>();
+
+            var notes = DedicatedStorage.GetComponents<MusicNoteComponent>();
+            var interactions = DedicatedStorage.GetComponents<MusicNoteInteractionComponent>();
+            var fillers = DedicatedStorage.GetComponents<MusicNoteFillerComponent>();
 
             for (int i = 0; i < DedicatedStorage.Count; i++)
             {
                 int entityId = DedicatedStorage.EntityIds[i];
                 GameObject view;
+
+                // Get or create appropriate view based on note type
                 if (notes[i].musicNoteType == MusicNoteType.LongNote)
                 {
                     view = longNoteViewFactory.GetOrCreateView(entityId, "longNote");
@@ -47,7 +50,11 @@ namespace ECS_MagicTile
                     view = shortNoteViewFactory.GetOrCreateView(entityId, "shortNote");
                 }
 
+                // Basic transform sync
                 SyncEntityToView(entityId, view);
+
+                // State sync
+                SyncNoteState(view, notes[i], interactions[i], fillers[i]);
             }
         }
 
@@ -62,6 +69,66 @@ namespace ECS_MagicTile
                 view.transform.localScale = transforms[index].Size;
                 // Sync other visual properties specific to MusicNote
             }
+        }
+
+        private void SyncNoteState(
+            GameObject view,
+            MusicNoteComponent note,
+            MusicNoteInteractionComponent interaction,
+            MusicNoteFillerComponent filler
+        )
+        {
+            // Get required components from view
+            var noteRenderer = view.GetComponent<SpriteRenderer>();
+            var fillerObject = view.transform.Find("Filler")?.gameObject;
+            var fillerRenderer = fillerObject?.GetComponent<SpriteRenderer>();
+
+            // Update visual state based on interaction state
+            switch (interaction.State)
+            {
+                case MusicNoteInteractiveState.Normal:
+                    noteRenderer.color = Color.white; // Default state
+                    break;
+                case MusicNoteInteractiveState.Pressed:
+                case MusicNoteInteractiveState.Hold:
+                    noteRenderer.color = Color.yellow; // Active state
+                    break;
+                case MusicNoteInteractiveState.Completed:
+                    noteRenderer.color = new Color(1, 1, 1, 0.5f); // Fade out completed notes
+                    break;
+            }
+
+            // Handle long note filler
+            if (
+                note.musicNoteType == MusicNoteType.LongNote
+                && fillerObject != null
+                && fillerRenderer != null
+            )
+            {
+                fillerObject.SetActive(filler.IsVisible);
+                if (filler.IsVisible)
+                {
+                    // Update filler visualization
+                    UpdateNoteFiller(fillerObject, fillerRenderer, filler.FillPercent);
+                }
+            }
+        }
+
+        private void UpdateNoteFiller(
+            GameObject fillerObj,
+            SpriteRenderer fillerRenderer,
+            float fillPercent
+        )
+        {
+            // Assuming filler uses a scale-based filling mechanism
+            Vector3 scale = fillerObj.transform.localScale;
+            scale.y = fillPercent;
+            // fillerObj.transform.localScale = scale;
+
+            SpriteUtility.ScaleFromPivot(fillerRenderer, scale, SpriteUtility.PivotPointXY.Bottom);
+
+            // Optional: Color gradient based on fill percentage
+            fillerRenderer.color = Color.Lerp(Color.yellow, Color.green, fillPercent);
         }
     }
 }
